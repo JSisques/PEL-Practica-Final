e852ec19b7618aaed7534ff3f47083729377eed6
JSisques <javierplazasisques@gmail.com>
2022-01-11 10:12:25 +0100

    Updated

--- main.cpp
+++ main.cpp
@@ -4,6 +4,8 @@
 
 using namespace std;
 
+//-------------VINILO-------------------
+
 class Vinilo {
    private:
     string titulo;
@@ -11,30 +13,92 @@ class Vinilo {
     list<string> instrumentos;
 
    public:
-    Vinilo(/* args */);
-    ~Vinilo();
+    Vinilo(/* args */){
+
+    };
+    ~Vinilo(){
+
+    };
+    void toString() {
+        cout << "Prueba" << endl;
+    };
+    ;
 };
+
+//-------------ARTISTA-------------------
 
 class Artista {
    private:
-    /* data */
+    string nombre;
+    string estilo;
+    list<string> instrumentosNecesarios;
+
    public:
-    Artista(/* args */);
-    ~Artista();
+    Artista(string nombre, string estilo, list<string> instrumentosNecesarios) {
+        this->nombre = nombre;
+        this->estilo = estilo;
+        this->instrumentosNecesarios = instrumentosNecesarios;
+    };
+    ~Artista(){
+
+    };
+    void toString() {
+        cout << "Prueba" << endl;
+    };
 };
 
-Artista::Artista(/* args */) {
-}
+//--------------GRABACION---------------
 
-Artista::~Artista() {
-}
+class Grabacion {
+   private:
+    Artista artista;
+    string estado;
+    string fecha;
 
-Vinilo::Vinilo(/* args */) {
-}
+   public:
+    Grabacion(Artista artista, string estado, string fecha) {
+        this->artista = artista;
+        this->estado = estado;
+        this->fecha = fecha;
+    };
 
-Vinilo::~Vinilo() {
-}
+    ~Grabacion(){
+
+    };
+    void toString() {
+        cout << "Prueba" << endl;
+    };
+};
+
+//--------------HISTORICO---------------
+
+class Historico {
+   private:
+    list<Grabacion> listadoGrabaciones;
+
+   public:
+    Historico(){
+
+    };
+    ~Historico(){
+
+    };
+
+    Grabacion addGrabacion(Grabacion grabacion) {
+        listadoGrabaciones.push_back(grabacion);
+    }
+
+    void toString() {
+        cout << "Prueba" << endl;
+    };
+};
 
 int main(int argc, char const *argv[]) {
+    Historico historico;
+
+    list<Artista> listadoArtistas;
+    list<Vinilo> listadoVinilos;
+    list<Grabacion> listadoGrabacion;
+
     return 0;
 }
